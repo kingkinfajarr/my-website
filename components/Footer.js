1a7d9e7d8bd38bc1dd00b113490e4f62fe99767b
kingkinfajarr <kingkinfajar8@gmail.com>
2022-05-12 23:01:19 +0700

    add link to redirect in footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,23 +3,23 @@ import Link from 'next/link';
 const Footer = () => {
   return (
     <footer className="">
-      <div className="text-center md:pb-10 md:pt-10 pb-14 font-semibold text-heading">
+      <div className="text-center md:pb-10 md:pt-10 pb-14 font-semibold leading-relaxed">
         <p>
           Created By{' '}
           <Link href={''}>
-            <span className="underline">Kingkin Fajar</span>
+            <a href="http://github.com/kingkinfajarr" target="_blank" rel="noopener noreferrer" className='underline'>Kingkin Fajar</a>
           </Link>{' '}
         </p>
-        <p>
+        <p className='leadi'>
           with{' '}
           <Link href={''}>
-            <span className="underline">Next JS,</span>
+            <a href="http://nextjs.org" target="_blank" rel="noopener noreferrer" className='underline'>Next JS,</a>
           </Link>{' '}
           <Link href={''}>
-            <span className="underline">Contentful,</span>
-          </Link>{' '}
+            <a href="http://contentful.com" target="_blank" rel="noopener noreferrer" className='underline'>Contentful</a>
+          </Link>{' '}and&nbsp;
           <Link href={''}>
-            <span className="underline">Tailwind CSS</span>
+            <a href="http://tailwindcss.com" target="_blank" rel="noopener noreferrer" className='underline'>Tailwind CSS</a>
           </Link>
         </p>
       </div>
