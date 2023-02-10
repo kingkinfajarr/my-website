8d45914b7c08d6d6bf4bac6186021dbd833933f4
kingkinfajarr <kingkinfajar8@gmail.com>
2023-02-10 16:36:07 +0700

    fix: auto-update with ISR (Incremental Static Regeneration)

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,15 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
 import { createClient } from "contentful";
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown from "react-markdown";
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_KEY
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
 });
 
 export const getStaticPaths = async () => {
-  const res = await client.getEntries({ content_type: 'blog' });
+  const res = await client.getEntries({ content_type: "blog" });
   const paths = res.items.map((item) => ({
     params: { slug: item.fields.slug },
   }));
@@ -22,42 +22,71 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async ({ params }) => {
   const { items } = await client.getEntries({
-    content_type: 'blog',
-    'fields.slug': params.slug,
+    content_type: "blog",
+    "fields.slug": params.slug,
   });
 
-  return { props: { blog: items[0] } };
+  return {
+    props: { blog: items[0] },
+    revalidate: 1,
+  };
 };
 
- const BlogDetail = ({ blog }) => {
+const BlogDetail = ({ blog }) => {
   const { title, thumbnail, content, tag } = blog.fields;
-   return (
-      <div className="flex md:min-h-screen flex-col items-center justify-center md:px-40 px-4">
-        <Head>
-          <title>{title}</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <main className="flex w-full flex-1 flex-col items-center justify-center text-center">
-          <div className="md:w-full md:h-96 h-64 bg-slate-300 rounded-lg bg-cover bg-center overflow-hidden md:mt-20">
-            <Image src={'https:' + thumbnail.fields.file.url} width={thumbnail.fields.file.details.image.width} height={thumbnail.fields.file.details.image.height} className="bg-cover bg-center" />
+  return (
+    <div className='flex md:min-h-screen flex-col items-center justify-center md:px-40 px-4'>
+      <Head>
+        <title>{title}</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main className='flex w-full flex-1 flex-col items-center justify-center text-center'>
+        <div className='md:w-full md:h-96 h-64 bg-slate-300 rounded-lg bg-cover bg-center overflow-hidden md:mt-20'>
+          <Image
+            src={"https:" + thumbnail.fields.file.url}
+            width={thumbnail.fields.file.details.image.width}
+            height={thumbnail.fields.file.details.image.height}
+            className='bg-cover bg-center'
+          />
+        </div>
+        <h1 className='md:text-4xl text-3xl font-bold md:my-7 my-4'>{title}</h1>
+        <ReactMarkdown
+          components={{
+            h1: (props) => (
+              <h1 className='text-left text-4xl font-bold'>{props.children}</h1>
+            ),
+            h2: (props) => (
+              <h2 className='text-left text-3xl font-bold'>{props.children}</h2>
+            ),
+            h3: (props) => (
+              <h3 className='text-left text-2xl mb-1 font-bold'>
+                {props.children}
+              </h3>
+            ),
+            hr: (props) => <hr className='mt-3 border-0' {...props.children} />,
+            p: (props) => (
+              <p className='text-left text-md leading-relaxed'>
+                {props.children}
+              </p>
+            ),
+            blockquote: (props) => (
+              <div className='text-left p-2 border-l-4 border-green-300 my-3'>
+                {props.children}
+              </div>
+            ),
+          }}
+          className='text-left'
+        >
+          {content}
+        </ReactMarkdown>
+        <div className='flex md:mt-5 mt-4 md:mb-0 mb-14'>
+          <div className='mr-2 bg-green-200 rounded px-2 py-1 font-medium'>
+            {tag}
           </div>
-          <h1 className="md:text-4xl text-3xl font-bold md:my-7 my-4">{title}</h1>
-          <ReactMarkdown components={{ 
-            h1: (props) => <h1 className="text-left text-4xl font-bold">{props.children}</h1>,
-            h2: (props) => <h2 className="text-left text-3xl font-bold">{props.children}</h2>,
-            h3: (props) => <h3 className="text-left text-2xl mb-1 font-bold">{props.children}</h3>,
-            hr: (props) => <hr className="mt-3 border-0" {...props.children}/>,
-            p: (props) => <p className="text-left text-md leading-relaxed">{props.children}</p>,
-            blockquote: (props) => <div className="text-left p-2 border-l-4 border-green-300 my-3">
-              {props.children}
-            </div>
-           }} className="text-left">{content}</ReactMarkdown>
-           <div className="flex md:mt-5 mt-4 md:mb-0 mb-14">
-            <div className='mr-2 bg-green-200 rounded px-2 py-1 font-medium'>{tag}</div>
-          </div>
-        </main>
-      </div>
-    );
- }
-  
- export default BlogDetail;
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default BlogDetail;
--- pages/projects.js
+++ pages/projects.js
@@ -1,76 +1,89 @@
-import Head from 'next/head';
+import Head from "next/head";
 import { createClient } from "contentful";
-import Card from '../components/projects/Card';
-import { NextSeo } from 'next-seo'
+import Card from "../components/projects/Card";
+import { NextSeo } from "next-seo";
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_KEY
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
 });
 
 export async function getStaticProps() {
-  const res = await client.getEntries({ content_type: "project" })
+  const res = await client.getEntries({ content_type: "project" });
 
   return {
-    props: { projects: res.items }
-  }
+    props: { projects: res.items },
+    revalidate: 1,
+  };
 }
 
 const Projects = ({ projects }) => {
-  return ( 
+  return (
     <>
-    <NextSeo
-      title='Kingkin Fajar Anifianto'
-      titleTemplate="Kingkin Fajar Anifianto"
-      defaultTitle="Kingkin Fajar Anifianto"
-      description="Hi, I'm Kingkin Fajar Developer and UI Design Enthusiast"
-      canonical="https://kingkinfajarr.my.id"
-      openGraph={{
-        type: "website",
-        url: "https://kingkinfajarr.my.id",
-        site_name: "Kingkin Fajar Anifianto",
-        title: "Kingkin Fajar Anifianto",
-        description: "Hi, I'm Kingkin Fajar Developer and UI Design Enthusiast",
-      }}
-      twitter={{
-        handle: "@kingkinfajarr",
-        site: "@kingkinfajarr",
-        cardType: "summary_large_image",
-      }}
-      additionalMetaTags={[
-        { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
-        { name: 'apple-mobile-web-app-title', content: 'Kingkin Fajar Anifianto' },
-        { name: 'application-name', content: 'Kingkin Fajar Anifianto' },
-        {
-          name: 'google-site-verification',
-          content: 'St3mL9W0jVuct39rx9f1NgYC6YTvTasfkjoE_l9T47k',
-        },
-        { name: 'msapplication-TileColor', content: '#da532c' },
-        { name: 'msapplication-config', content: '/favicon/browserconfig.xml' },
-        { name: 'theme-color', content: '#4ADE80' },
-      ]}
-    />
-    <div className='flex min-h-screen flex-col items-center justify-center md:px-40 px-4'>
-      <Head>
-        <title>Project | Kingkin Fajar</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className='flex w-full flex-1 flex-col justify-center text-center'>
-        <h1 className="md:text-lg text-base text-center md:mt-20 uppercase text-green-400">Projects</h1>
-        <div className="md:max-w-2xl mx-auto">
-          <h3 className='md:text-4xl text-2xl font-semibold tracking-wide mt-2 capitalize'>A Few Of My Favorite Works</h3>
-        </div>
-        <div className="grid md:gap-8 gap-4 md:grid-cols-2 auto-rows-auto md:mb-0 mb-16 md:mt-12 mt-8">
-          {projects.map(project => {
-            return (
-              <Card key={project.sys.id} project={project} />
-            );
-          })}
-        </div>
-      </main>
-    </div>
+      <NextSeo
+        title='Kingkin Fajar Anifianto'
+        titleTemplate='Kingkin Fajar Anifianto'
+        defaultTitle='Kingkin Fajar Anifianto'
+        description="Hi, I'm Kingkin Fajar Developer and UI Design Enthusiast"
+        canonical='https://kingkinfajarr.my.id'
+        openGraph={{
+          type: "website",
+          url: "https://kingkinfajarr.my.id",
+          site_name: "Kingkin Fajar Anifianto",
+          title: "Kingkin Fajar Anifianto",
+          description:
+            "Hi, I'm Kingkin Fajar Developer and UI Design Enthusiast",
+        }}
+        twitter={{
+          handle: "@kingkinfajarr",
+          site: "@kingkinfajarr",
+          cardType: "summary_large_image",
+        }}
+        additionalMetaTags={[
+          {
+            name: "viewport",
+            content: "width=device-width, initial-scale=1.0",
+          },
+          {
+            name: "apple-mobile-web-app-title",
+            content: "Kingkin Fajar Anifianto",
+          },
+          { name: "application-name", content: "Kingkin Fajar Anifianto" },
+          {
+            name: "google-site-verification",
+            content: "St3mL9W0jVuct39rx9f1NgYC6YTvTasfkjoE_l9T47k",
+          },
+          { name: "msapplication-TileColor", content: "#da532c" },
+          {
+            name: "msapplication-config",
+            content: "/favicon/browserconfig.xml",
+          },
+          { name: "theme-color", content: "#4ADE80" },
+        ]}
+      />
+      <div className='flex min-h-screen flex-col items-center justify-center md:px-40 px-4'>
+        <Head>
+          <title>Project | Kingkin Fajar</title>
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <main className='flex w-full flex-1 flex-col justify-center text-center'>
+          <h1 className='md:text-lg text-base text-center md:mt-20 uppercase text-green-400'>
+            Projects
+          </h1>
+          <div className='md:max-w-2xl mx-auto'>
+            <h3 className='md:text-4xl text-2xl font-semibold tracking-wide mt-2 capitalize'>
+              A Few Of My Favorite Works
+            </h3>
+          </div>
+          <div className='grid md:gap-8 gap-4 md:grid-cols-2 auto-rows-auto md:mb-0 mb-16 md:mt-12 mt-8'>
+            {projects.map((project) => {
+              return <Card key={project.sys.id} project={project} />;
+            })}
+          </div>
+        </main>
+      </div>
     </>
   );
-}
- 
+};
+
 export default Projects;
