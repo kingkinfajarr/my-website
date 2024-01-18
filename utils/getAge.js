59244a2ab2b5179c391d0ab2bb06aeb1937522e5
kingkinfajarr <kingkinfajar8@gmail.com>
2024-01-18 23:09:43 +0700

    migrate to v2

--- utils/getAge.js
+++
@@ -1,8 +0,0 @@
-const getAge = () => {
-  const dateNow = Date.now() - new Date('2003, 09, 11').getTime();
-  const age = new Date(dateNow);
-
-  return Math.abs(age.getUTCFullYear() - 1970);
-};
- 
-export default getAge;
